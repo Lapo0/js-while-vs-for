6b35ac80cef2992ba4761d6a305499798235e8f4
Lapo0 <lapo.gini.01@gmail.com>
2023-02-07 15:45:29 +0100

    FEAT valori in console singoli

--- js/app.js
+++ js/app.js
@@ -1,27 +1,24 @@
-console.log('funzia?')
+// console.log('funzia?')
 
-// Chiede un numero all'utente
 let numeroDispari
 
 let lista = []
 
+// Ripetere il ciclo fino al raggiungimento di 10 cifre
 for (i = 0; i < 10; i++) {
 
+    // Chiede un numero all'utente
     numeroDispari = parseInt(prompt('Inserisci un numero dispari'))
 
+    // Se il numero è dispari da salvare nell'array altrimenti scartare 
     while (isNaN(numeroDispari) || (numeroDispari % 2 === 0)) {
         numeroDispari = parseInt(prompt('Inserisci un numero dispari'))
     }
+    // Stampare i valori in console uno alla volta
+    console.log(numeroDispari)
+
+    // Aggiungere i valori all'array
     lista.push(numeroDispari)
 }
 
-
 console.log(lista, numeroDispari)
-
-
-
-// Ripetere il ciclo fino al raggiungimento di 10 cifre
-
-// Se il numero è dispari da salvare nell'array altrimenti scartiamo 
-
-// Stampare i valori in cnsole uno alla volta
