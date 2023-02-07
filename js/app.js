950d7a82cd6bc6c91876569996f62d7546a1dc04
Lapo0 <lapo.gini.01@gmail.com>
2023-02-07 15:23:26 +0100

    FEAT ciclo while per numero dispari

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 console.log('funzia?')
+
+// Chiede un numero all'utente
+let numeroDispari
+
+numeroDispari = parseInt(prompt('Inserisci un numero dispari'))
+
+while (isNaN(numeroDispari) || (numeroDispari % 2 === 0)) {
+    numeroDispari = parseInt(prompt('Inserisci un numero dispari'))
+}
+
+console.log(numeroDispari)
+
+// Ripetere il ciclo fino al raggiungimento di 10 cifre
+
+// Se il numero è dispari da salvare nell'array altrimenti scartiamo 
+
+// Stampare i valori in cnsole uno alla volta
